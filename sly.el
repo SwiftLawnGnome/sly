b9e464ed73af4902117fc13197a1a5527208a09f
Zach Shaftel <zshaftel@gmail.com>
2020-05-20 18:45:54 -0400

    Several small performance tweaks

--- sly.el
+++ sly.el
@@ -1595,13 +1595,9 @@ EVAL'd by Lisp."
                         (fboundp sly-net-send-translator))
                    (funcall sly-net-send-translator sexp)
                  sexp))
-         (payload (let (print-escape-nonascii
-                        print-escape-newlines
-                        print-length
-                        print-level)
-                    (encode-coding-string
-                     (concat (prin1-to-string sexp) "\n")
-                     'utf-8-unix)))
+         (payload (encode-coding-string
+                   (concat (sly-prin1-to-string sexp) "\n")
+                   'utf-8-unix))
          (string (concat (sly-net-encode-length (length payload))
                          payload)))
     (sly-log-event sexp proc)
@@ -1719,6 +1715,15 @@ EVAL'd by Lisp."
 
 (defun sly-net-encode-length (n)
   (format "%06x" n))
+
+(defun sly-prin1-to-string (sexp)
+  "Like `prin1-to-string' but don't octal-escape non-ascii characters.
+This is more compatible with the CL reader."
+  (let (print-escape-nonascii
+        print-escape-newlines
+        print-length
+        print-level)
+    (prin1-to-string sexp)))
 
 
 ;;;; Connections
